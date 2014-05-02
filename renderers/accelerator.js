6712a0ae02a0d7f8be306b04df30d09dc227cd41
Siedrix <Siedrix@gmail.com>
2014-05-02 02:39:38 -0500

    Added data from admin to accelerator page

--- renderers/accelerator.js
+++ renderers/accelerator.js
@@ -1,10 +1,34 @@
-var db = require('../lib/db');
+var db = require('../lib/db'),
+	_ = require('underscore');
 
 var Startup = db.model('startup');
+var Page = db.model('page');
+var User = db.model('user');
 
 var render = function (req, res) {
-	res.render('renderers/accelerator',{
-		currentPage : 'accelerator'
+	Page.findOne({name:'accelerator'}).populate('batch').exec(function(err, page){
+		if(err){ return res.send(500, err);}
+
+		var startupSlugs = [];
+		var mentorSlugs = [];
+
+		if(!page.data){page.data = {};}
+		if(page.data.startups){startupSlugs = startupSlugs.concat(page.data.startups.split(','));}
+		if(page.data.mentors){mentorSlugs = mentorSlugs.concat(page.data.mentors.split(','));}
+
+		Startup.find({slugStr:{$in:startupSlugs}}, function (err, startups) {
+			User.find({slugStr:{$in:mentorSlugs}, type:'mentor'}, function(err, mentors){
+				mentors = _.sortBy(mentors, function(item){return page.data.mentors.split(',').indexOf(item.slugStr);});
+				startups = _.sortBy(startups, function(item){return page.data.startups.split(',').indexOf(item.slugStr);});
+
+				res.render('renderers/accelerator',{
+					page : page,
+					mentors : mentors,
+					startups : startups,
+					currentPage : 'accelerator'
+				});
+			});
+		});
 	});
 };
 
