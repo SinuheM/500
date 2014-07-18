940fee36b201931f36211adad0b041c9adc49330
Leonidas Esteban <leonidasesteban@gmail.com>
2014-07-18 01:29:13 -0500

    first commit, css to stylus and accelerator ipad responsive

--- static/js/500.js
+++ static/js/500.js
@@ -1,14 +1,14 @@
 $(function () {
     var msie6 = $.browser == 'msie' && $.browser.version < 7;
-	    if (!msie6) {
-		    var top = $('#fixadent').offset().top - parseFloat($('#fixadent').css('margin-top').replace(/auto/, 0));
-		    $(window).scroll(function (event) {
-		    var y = $(this).scrollTop();
-		    if (y >= top) {
-		    $('#fixadent').addClass('fixed');
-		    } else {
-		    $('#fixadent').removeClass('fixed');
-		    }
-	    });
+        if (!msie6) {
+            var top = $('#fixadent').offset().top - parseFloat($('#fixadent').css('margin-top').replace(/auto/, 0));
+            $(window).scroll(function (event) {
+            var y = $(this).scrollTop();
+            if (y >= top) {
+            $('#fixadent').addClass('fixed');
+            } else {
+            $('#fixadent').removeClass('fixed');
+            }
+        });
     }
-}); 
+});
